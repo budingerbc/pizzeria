fb82b30585319104401a872da3976ecd34ea2d5c
Ben Budinger <budingerbc@gmail.com>
2017-07-18 13:22:58 -0700

    setup website with banner logo and columns for pizza order and subtotal menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
+
 $(document).ready(function() {
   $("").submit(function(event) {
     event.preventDefault();
