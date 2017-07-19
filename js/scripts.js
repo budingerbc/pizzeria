f1215d245c48ab4ccdc0f38fd8a238cdef026f75
Ben Budinger <budingerbc@gmail.com>
2017-07-19 09:43:44 -0700

    implement pizza object and properties, as well as back-end logic for determining size and topping prices, as well as business-facing logic for pulling data values

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+function Order(pizza) {
+
+}
+
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
@@ -5,26 +9,42 @@ function Pizza(size, toppings) {
   this.toppingPrice = 0;
 }
 
-Pizza.prototype.setCosts() {
-  if (size === "small") {
+Pizza.prototype.setCosts = function() {
+  if (this.size === "small") {
     this.basePrice = 9.99;
-    this.toppingPrice = 0.5
-  } else if (size === "medium") {
+    this.toppingPrice = 0.50;
+  } else if (this.size === "medium") {
     this.basePrice = 10.99;
-    this.toppingPrice = 0.75
+    this.toppingPrice = 0.75;
   } else {
     this.basePrice = 12.99;
-    this.basePrice = 1.00;
+    this.toppingPrice = 1.00;
   }
-}
+};
+
+Pizza.prototype.getSubtotal = function() {
+  return this.basePrice + this.toppingPrice * this.toppings.length;
+};
 
 $(document).ready(function() {
   $("form#pizza-order").submit(function(event) {
     event.preventDefault();
 
-    alert("is this working");
-    var order = new Pizza("medium");
-    order.setCosts();
-    alert(order.basePrice);
+    var pizzaSize = $("input:radio[name=pizza-size]:checked").val();
+    var pizzaToppings = [];
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      pizzaToppings.push($(this).val());
+    });
+
+    if (pizzaSize !== undefined) {
+      var order = new Pizza(pizzaSize, pizzaToppings);
+      order.setCosts();
+      alert(order.getSubtotal());
+
+    } else {
+      alert("Please select a size for your pizza.");
+    }
+
+
   });
 });
